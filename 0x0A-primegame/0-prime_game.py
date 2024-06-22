da2ac61cac7eb8046bb6a6c722a4e7b8e9c2f42f
Isabel <18isabelbanda@gmail.com>
2024-06-22 01:09:13 +0100

    Fixed prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,67 +1,39 @@
-
 #!/usr/bin/python3
+"""Prime Game
 """
-This module contains the function `isWinner`
-which determines the winner of the game.
-"""
-
-
-def is_prime(num):
-    """
-    Checks if a number is prime.
-    Returns True if the number is prime, False otherwise.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
 
 
 def isWinner(x, nums):
+    """Prime Game
     """
-    Determines the winner of the game based on
-    the number of rounds and the values of `nums`.
-
-    Args:
-        x (int): The number of rounds.
-        nums (list): A list of integers representing the
-        values of `n` for each round.
-
-    Returns:
-        str or None: The name of the player with the
-        most wins (either "Maria" or "Ben"),
-        or None if the winner cannot be determined.
-    """
-    maria_wins = 0
-    ben_wins = 0
-
-    for i in range(x):
-        primes = []
-        for j in range(2, nums[i] + 1):
-            if is_prime(j):
-                primes.append(j)
-
-        m_turn = len(primes) % 2 == 0
-        b_turn = not m_turn
-
-        while primes:
-            if m_turn:
-                primes.pop()
-            else:
-                primes.pop(0)
-            m_turn = not m_turn
-            b_turn = not b_turn
-
-        if m_turn:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if x < 1 or not nums:
         return None
+
+    n = max(nums)
+
+    primes = [True for _ in range(max(n + 1, 2))]
+    primes[0], primes[1] = False, False
+
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+
+    primes = [i for i, p in enumerate(primes) if p]
+
+    wins = 0
+    for n in nums:
+        wins += sum(p <= n for p in primes) % 2 == 1
+
+    if wins * 2 == len(nums):
+        return None
+
+    if wins * 2 > len(nums):
+        return "Maria"
+
+    return "Ben"
+
+
+# print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
